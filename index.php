33fd4f03e755c72a4d9278178c7c773b118e0f8d
miguel <espinoza@12>
2016-02-29 20:38:37 -0500

    gaspar soca

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>HOLA PROFESOR  TRIGOSO  </title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<img src="graxi.gif" alt="">
+<marquee>
+<h1>GRACIELA GASPAR SOCA </h1>
+<marquee>
+	
+	
+</body>
+</html>
